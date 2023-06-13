d46d8ce66b09ccc08415f8f81d08c62dd1ee84ae
Borys Menko <borysmenko@pingidentity.com>
2023-06-13 14:21:42 +0300

    compose updated

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from project import create_app
+import os
+import logging
 
 app = create_app()
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    print(os.getenv("DEBUG_MODE"))
+    print("Welcome Home _______------____")
+    app.run(debug=os.getenv("DEBUG_MODE"))
